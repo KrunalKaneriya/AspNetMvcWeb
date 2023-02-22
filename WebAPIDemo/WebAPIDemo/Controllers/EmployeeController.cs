63792a692c0d3e3b87e5306a861bc1bf8e0d5485
KrunalKaneriya <prokillerkk@outlook.com>
2023-02-23 00:05:54 +0530

    GET API with id not working js error

---
+++ WebAPIDemo/WebAPIDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    
+    public class EmployeeController : ApiController
+    {
+
+        public List<Employee>Get()
+        {
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+                return context.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+                Employee existingEmployee = context.Employees.Find(id);
+                Console.Write(existingEmployee.ToString());
+                return existingEmployee;
+            }
+        }
+
+
+        public string Post([FromBody] Employee emp)
+        {
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+                context.Employees.Add(emp);
+                context.SaveChanges();
+                return "data inserted post api";
+            }
+        }
+
+        public HttpResponseMessage Put([FromBody] Employee emp)
+        {
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+                try
+                {
+                    Employee existingEmployee = context.Employees.Find(emp.Id);
+                    existingEmployee.firstname = emp.firstname;
+                    existingEmployee.lastname = emp.lastname;
+                    existingEmployee.gender = emp.gender;
+                    existingEmployee.salary = emp.salary;
+                    context.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, emp);
+                } catch(Exception e)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+                        
+                }
+               
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+                try
+                {
+                    Employee emp = context.Employees.Find(id);
+                    context.Employees.Remove(emp);
+                    context.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, emp);
+                } catch (Exception e)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ WebAPIDemo/WebAPIDemo/Controllers/EmployeeMVCController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeeMVCController : Controller
+    {
+        EmployeeEntities context = new EmployeeEntities();
+        // GET: EmployeeMVC
+        public ActionResult Index()
+        {
+            return View("Index",context.Employees.ToList());
+        }
+
+        public ActionResult Find()
+        {
+            return View("Find");
+        }
+    }
+}
